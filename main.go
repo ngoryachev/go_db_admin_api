0052a05d4fae116ee57fdc4b6a5e4a4f8b378d0e
Nick Goriachev <ngoryacheff@gmail.com>
2024-06-13 22:54:22 +0400

    naiv

--- main.go
+++ main.go
@@ -5,23 +5,37 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-var (
-	user     = os.Getenv("DB_USER")
-	password = os.Getenv("DB_PASSWORD")
-	host     = os.Getenv("DB_HOST")
-	port     = os.Getenv("DB_PORT")
-	dbname   = os.Getenv("DB_NAME")
-
-	DSN = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8", user, password, host, port, dbname)
-)
-
 func main() {
-	db, err := sql.Open("mysql", DSN)
-	err = db.Ping() // вот тут будет первое подключение к базе
+	// Получение переменных окружения
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	dbname := os.Getenv("DB_NAME")
+
+	// Формирование DSN строки
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8", user, password, host, port, dbname)
+
+	var db *sql.DB
+	var err error
+
+	// Повторные попытки подключения к базе данных
+	for i := 0; i < 10; i++ {
+		db, err = sql.Open("mysql", dsn)
+		if err == nil {
+			err = db.Ping() // вот тут будет первое подключение к базе
+			if err == nil {
+				break
+			}
+		}
+		fmt.Println("Не удалось подключиться к базе данных. Повторная попытка через 5 секунд...")
+		time.Sleep(5 * time.Second)
+	}
 	if err != nil {
 		panic(err)
 	}
