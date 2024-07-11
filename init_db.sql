84220ce6de9c7dfdaa4013263bf64117066b7790
Nick Goriachev <ngoryacheff@gmail.com>
2024-07-11 23:23:21 +0400

    naiv

--- init_db.sql
+++ init_db.sql
@@ -1,35 +1,47 @@
--- Adminer 4.2.1 MySQL dump
-
 SET NAMES utf8;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-DROP TABLE IF EXISTS `items`;
-CREATE TABLE `items` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
-  `description` text NOT NULL,
-  `updated` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS `agents`;
+CREATE TABLE `agents` (
+    `agent_id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `email` varchar(255) NOT NULL,
+    `name` varchar(255),
+    `phone` varchar(255)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `items` (`id`, `title`, `description`, `updated`) VALUES
-(1,	'database/sql',	'Рассказать про базы данных',	'rvasily'),
-(2,	'memcache',	'Рассказать про мемкеш с примером использования',	NULL);
+DROP TABLE IF EXISTS `operations`;
+CREATE TABLE `operations` (
+    `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `operation_type` int NOT NULL,
+    `product_id` int NOT NULL,
+    `agent_id` int NOT NULL,
+    `place_id_from` int,
+    `place_id_to` int,
+    `at` varchar(255) NOT NULL,
+    `count` int NOT NULL,
+    `price_actual` int NOT NULL
+   ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `login` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `info` text NOT NULL,
-  `updated` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
+DROP TABLE IF EXISTS `places`;
+CREATE TABLE `places` (
+    `place_id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name` varchar(255) NOT NULL,
+    `is_supplier` int NOT NULL,
+    `is_storage` int NOT NULL,
+    `is_retail` int NOT NULL,
+    `address` varchar(255),
+    `phone` varchar(255),
+    `description` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `users` (`user_id`, `login`, `password`, `email`, `info`, `updated`) VALUES
-(1,	'rvasily',	'love',	'rvasily@example.com',	'none',	NULL);
-
--- 2017-11-22 23:33:12
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products` (
+    `product_id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `place_id_of_supplier` int NOT NULL,
+    `price` int NOT NULL,
+    `name` varchar(255) NOT NULL,
+    `vendor_code` varchar(255),
+    `author` varchar(255)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
